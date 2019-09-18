6513430e61c04d1f29554c5146353ce8ce719891
Will Wallace <will.wallace@simplivity.com>
2019-09-17 21:19:07 -0400

    Adding nested_loops.py

---
+++ nested_loops.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+from __future__ import print_function
+
+
+for x in range(1, 5):
+    print ("X", x, "Bleeeeeeeeeep")
+    for y in range(1, 5):
+        print("Y", y, "Bloop")
+print("Fzzzzt")
