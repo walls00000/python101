e1b69174fa0672de297e9494fe547cf879c27434
Will Wallace <walls00000@gmail.com>
2020-12-03 00:49:15 -0500

    Adding more ciphers and encodings to CipherUtils.py

--- oo_python/Ciphers/CipherUtils.py
+++ oo_python/Ciphers/CipherUtils.py
@@ -1,3 +1,5 @@
+import math
+
 class CipherUtils:
 
     '''
@@ -9,7 +11,45 @@ class CipherUtils:
         ''' Constructor for CipherUtils '''
         self.alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
-    def getAlphabetLength(self):
+    def binaryEncode(self, ascii_string):
+        ''' Dncode an ascii string into binary '''
+        binary = bin(int.from_bytes(ascii_string.encode(), 'big'))
+        return binary
+
+    def binaryDecode(self, binary):
+        ''' Decode a binary encoded message into an ascii string '''
+        n = 0
+        try:
+            n = int(binary, 2)
+        except:
+            print("Error: input is not binary")
+        decoded = n.to_bytes((n.bit_length() + 7) // 8, 'big').decode()
+        return decoded
+
+    def asciiEncode(self, message):
+        ''' Encode an ascii message into a string of each character's integer value '''
+        encoded = ""
+        for char in message:
+            value = ord(char)
+            formatted = "{}".format(f"{value:03d}")
+            encoded += formatted
+        return encoded
+
+    def asciiDecode(self, encoded):
+        ''' Decode an ascii encoded message into ascii characters '''
+        if len(encoded) % 3 != 0:
+            raise Exception("Message not ascii encoded")
+        message = ""
+        for i in range(0, len(encoded), 3):
+            value="{}{}{}".format(encoded[i], encoded[i+1], encoded[i+2])
+            try:
+                message += chr(int(value))
+            except:
+                raise Exception ("Value is not an integer: '{}'".format(value))
+        return message
+
+    def getCaesarAlphabetLength(self):
+        ''' Return the length of the caesar alphabet '''
         return self.alphabet.__len__()
 
     def caesarCipher(self, clearText, shiftAmount):
@@ -28,9 +68,71 @@ class CipherUtils:
 
         for currentCharacter in clearText:
             position = self.alphabet.find(currentCharacter)
-            newPosition = (position + shiftAmount) % self.getAlphabetLength()
+            newPosition = (position + shiftAmount) % self.getCaesarAlphabetLength()
             if currentCharacter in self.alphabet:
                 encryptedString = encryptedString + self.alphabet[newPosition]
             else:
                 encryptedString = encryptedString + currentCharacter
         return encryptedString
+
+    def transposeEncrypt(self, key, message):
+        '''  Use the transpose cipher to encrypt a message using a key for column length '''
+        # Each string in ciphertext represents a column in the grid.
+        ciphertext = [''] * key
+        #Loop through each column in ciphertext
+        for column in range(key):
+            currentIndex = column
+
+            #Keep looping until currentIndex goes past the message length.
+            while currentIndex < len(message):
+                # Place the character at currentIndex in message at the
+                # end of the current column in the ciphertext list:
+                ciphertext[column] += message[currentIndex]
+
+                #Move currentIndex over.
+                currentIndex += key
+
+        # Convert the ciphertext list into a single string value and return it
+        return ''.join(ciphertext)
+
+    def transposeDecrypt(self, key, message):
+        '''  Decrypt a transpose encrypted message given the column key '''
+        # The transposition decrypt function will simulate the "columns" and
+        # "rows" of the grid that the plaintext is written on by using a list
+        # of strings. First we need to calculate a few values.
+
+        # The number of "cplumns" in our transposition grid.
+        numOfColumns = int(math.ceil(len(message) / float(key)))
+        numOfRows = key
+
+        # The number of "shaded boxes" in the last "column" of the grid.
+        numOfShadedBoxes = (numOfColumns * numOfRows) - len(message)
+
+        # Each string in plaintext represents a column in the grid
+        plaintext = [''] * numOfColumns
+
+        # The column and row variables point to where in the grid the next
+        # character in the encrypted message will go.
+        column = 0
+        row = 0
+
+        for symbol in message:
+            plaintext[column] += symbol
+            column += 1 # Point to the next column.
+
+            # If there are no more columns OR we're at a shaded box, go back
+            # to the first column and the next row.
+            if(column == numOfColumns) or (column == numOfColumns-1 and
+            row >= numOfRows - numOfShadedBoxes):
+                column = 0
+                row += 1
+        return ''.join(plaintext)
+
+    def reverseCipher(self,message):
+        ''' Reverse the message and return it '''
+        i = len(message) - 1
+        translated = ""
+        while i >= 0:
+            translated = translated + message[i]
+            i = i - 1
+        return translated
--- oo_python/testCiphers.py
+++ oo_python/testCiphers.py
@@ -9,12 +9,65 @@ from Ciphers.CipherUtils import CipherUtils
 message = "I love to encrypt things"
 cipherUtils = CipherUtils()
 
-def testAlphabetLength():
-    actual = cipherUtils.getAlphabetLength()
+def testBinaryEncode():
+    ''' Verify binaryEncode encodes ascii 'A' properly '''
+    actual = cipherUtils.binaryEncode("A")
+    expected = "0b1000001"
+    assert actual == expected
+
+def testBinaryDecode():
+    ''' Verify binaryDecode can decode ascii 'A' '''
+    actual = cipherUtils.binaryDecode("0b1000001")
+    expected = "A"
+    assert actual == expected
+
+def testBinaryDecodeInvalidInput():
+    ''' Verify binaryDecode throws an exception when input is not binary '''
+    with pytest.raises(Exception):
+        assert cipherUtils.binaryDecode("A")
+
+def testBinaryEncodeDecode():
+    ''' Verify encode/decode results in the original input '''
+    encoded = cipherUtils.binaryEncode(message)
+    decoded = cipherUtils.binaryDecode(encoded)
+    assert decoded == message
+
+def testAsciiEncode():
+    ''' Verify asciiEncode can encode '''
+    expected = "073032108111118101032116111032101110099114121112116032116104105110103115"
+    encoded = cipherUtils.asciiEncode(message)
+    assert encoded == expected
+
+def testAsciiDecode():
+    ''' Verify asciiDecode can decode '''
+    encoded = "073032108111118101032116111032101110099114121112116032116104105110103115"
+    decoded = cipherUtils.asciiDecode(encoded)
+    assert decoded == message
+
+def testAsciiDecodeInvalidNonMultiple3():
+    ''' Verify asciiDecode throws on sting length which is not a multiple of 3 '''
+    with pytest.raises(Exception):
+        assert cipherUtils.asciiDecode("0044")
+
+def testAsciiDecodeInvalidNonInteger():
+    ''' Verify asciiDecode throws on non-integer input '''
+    with pytest.raises(Exception):
+        assert cipherUtils.asciiDecode("00300A")
+
+def testAsciiEncodeDecode():
+    '''  Verify asciiEncoded messages can be decoded'''
+    encoded = cipherUtils.asciiEncode(message)
+    decoded = cipherUtils.asciiDecode(encoded)
+    assert decoded == message
+
+def testCaesarAlphabetLength():
+    ''' Verify caesar cipher alphabet length is 62 '''
+    actual = cipherUtils.getCaesarAlphabetLength()
     expected = 62
     assert actual == expected, "failed: expected {} but got {}".format(expected, actual)
 
 def testCaesarCipher():
+    ''' Verify caesar cipher encryption of a message '''
     encrypted = cipherUtils.caesarCipher(message, 7)
     expected = "P svCl Av lujyFwA Aopunz"
     assert encrypted == expected, "failed: expected '{}' but got '{}'".format(expected, encrypted)
@@ -23,9 +76,64 @@ def testCaesarCipher():
     assert decrypted == message, "failed: expected '{}' but got '{}'".format(message, decrypted)
 
 def testAllCaesarKeys():
-    for i in range(1,cipherUtils.getAlphabetLength()):
+    ''' Verify encryption and decryption of all keys'''
+    n = 0 - cipherUtils.getCaesarAlphabetLength()
+    for i in range(n, cipherUtils.getCaesarAlphabetLength() + 1):
         decryptKey=0-i
         encrypted = cipherUtils.caesarCipher(message,i)
         decrypted = cipherUtils.caesarCipher(encrypted,decryptKey)
         assert decrypted == message, "failed: Iteration with key {} and decryption key {}"\
             .format(i, decryptKey)
+
+def testOutOfRangeCipherKey():
+    ''' Verify that a key outside the range of the alphabet is converted to a predictable key'''
+    key = 2348392354
+    decryptKey = 0 - key
+    encrypted = cipherUtils.caesarCipher(message, key)
+    assert encrypted == "K nqxg vq gpetArv vjkpiu"
+    decrypted = cipherUtils.caesarCipher(encrypted, decryptKey)
+    assert decrypted == message, "failed: encryption/decryption with key {} and decryption key {}" \
+        .format(key, decryptKey)
+
+def testTransposeEncrypt():
+    ''' Verify transposeEncrypt will encrypt a message '''
+    encrypted = cipherUtils.transposeEncrypt(8,message)
+    assert encrypted == "Iot   letonhvciern ygtps"
+
+def testTransposeDecrypt():
+    ''' Veryfy transposeDecrypt will decrypt a message '''
+    decrypted = cipherUtils.transposeDecrypt(8, "Iot   letonhvciern ygtps")
+    assert decrypted == message
+
+def testTranspopseEncrypt1():
+    "Verify using a column key of 1 will result in the original message"
+    encrypted = cipherUtils.transposeEncrypt(1,message)
+    assert encrypted == message
+def testTransposeEncryptDecrypt():
+    ''' Verify transposeEncrypted messagges can be decrypted by transposeDecrypt with various keys '''
+    for key in range(2,17):
+        encrypted = cipherUtils.transposeEncrypt(key, message)
+        decrypted = cipherUtils.transposeDecrypt(key, encrypted)
+        assert encrypted != message, "Failed for key: {}".format(key)
+        assert decrypted == message
+
+def testTransposeEncryptKeyMax():
+    ''' Verify transposeEncrypt with a key value >= results in the original message '''
+    key = len(message)
+    encrypted = cipherUtils.transposeEncrypt(key, message)
+    assert encrypted == message
+    key2 = len(message)+1
+    encrypted = cipherUtils.transposeEncrypt(key2, message)
+    assert encrypted == message
+
+def testTransposeDecryptPlainText():
+    ''' Verify transposeDecrypt on plain text will not throw '''
+    decrypted = cipherUtils.transposeDecrypt(8, message)
+    assert decrypted == "Io  cptn vterthgleony is"
+
+def testReverseCipher():
+    ''' Verify the reverse cipher can encrypt and decrypt '''
+    encrypted = cipherUtils.reverseCipher(message)
+    decrypted = cipherUtils.reverseCipher(encrypted)
+    assert encrypted == "sgniht tpyrcne ot evol I"
+    assert decrypted == message
